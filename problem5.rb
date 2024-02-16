db973dc32b9163f03114952f8146d2c061d0ec30
Jeralyn Saenz <saenz.388j@gmail.com>
2024-02-16 12:07:53 -0600

    Custom exception

---
+++ problem5.rb
@@ -0,0 +1,18 @@
+
+class InvalidAgeError < StandardError
+end
+
+def validate_age(age)
+  if age < 0
+    raise InvalidAgeError, "Age cannot be negative."
+  else
+    puts "Age is valid"
+  end
+end
+
+begin
+  validate_age(-5)
+  validate_age(30)
+rescue InvalidAgeError => e
+  puts "InvalidAgeError: #{e.message}"
+end
